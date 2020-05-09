276dd69100e4b03da66a8e7d719fdd6fe3c9496b
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-05-09 10:21:29 -0600

    Add code to book.js that fulfills book-test.js requirements.

--- src/book.js
+++ src/book.js
@@ -1,12 +1,47 @@
-function createTitle() {
-  
+function createTitle(workingTitle) {
+  return "The " + workingTitle;
+};
+
+function buildMainCharacter(name, age, pronouns) {
+  return {
+    name: name,
+    age: age,
+    pronouns: pronouns,
+  }
+};
+
+function saveReview(bookReview, reviews) {
+  var isNewReview = reviews.indexOf(bookReview) === -1
+  if (isNewReview) {
+    return reviews.push(bookReview);
+  }
+};
+
+function calculatePageCount(bookTitle) {
+  return bookTitle.length * 20;
+};
+
+function writeBook(bookTitle, bookCharacter, bookGenre) {
+  var bookPageCount = calculatePageCount(bookTitle)
+  var bookObject = {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: bookPageCount,
+    genre: bookGenre,
+  };
+  return bookObject;
+};
+
+function editBook(ghoulBook) {
+  ghoulBook.pageCount = ghoulBook.pageCount * 3/4;
+  return ghoulBook.pageCount
 }
 
 module.exports = {
   createTitle: createTitle,
-  // buildMainCharacter: buildMainCharacter,
-  // saveReview: saveReview,
-  // calculatePageCount: calculatePageCount,
-  // writeBook: writeBook,
-  // editBook: editBook
+  buildMainCharacter: buildMainCharacter,
+  saveReview: saveReview,
+  calculatePageCount: calculatePageCount,
+  writeBook: writeBook,
+  editBook: editBook
 }
